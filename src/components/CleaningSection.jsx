c6822b1bda7899837467971b2781fdafe11b16b3
Christopher Wright <christopherwright@Christophers-MacBook-Pro.local>
2025-08-31 22:32:08 +0100

    Fix firebase import path in CleaningSection

--- src/components/CleaningSection.jsx
+++ src/components/CleaningSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { db } from "./firebase";
+import { db } from "../firebase";
 import { collection, addDoc, getDocs, query, orderBy } from "firebase/firestore";
 
 const defaultTasks = [
